11c130d512284d5b18e90ad9c1eccc216ce57e2c
x-zero <v_sigma@hotmail.com>
2014-01-29 23:54:19 -0500

    d
    d

--- RPCTest/RPCTest/HelloWorldService.cs
+++ RPCTest/RPCTest/HelloWorldService.cs
@@ -12,16 +12,9 @@ namespace RPCTest
         {
             return "Hello World " + message;
         }
-        /*
-        [JsonRpcMethod]
-        private List<String> lista(string item)
-        {
-            List<String> lista1= new List<string>();
-            lista1.Add("uno");
-            lista1.Add("dos");
-            lista1.Add("tres");
 
-        }*/
+
+
 
 
 
