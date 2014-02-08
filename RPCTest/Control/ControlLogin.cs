b80c2c21e88be8eb59da7b0ea864d699e1c9ca2d
Wilmer Cotrina Barzola <v_sigma@hotmail.com>
2014-02-08 16:27:14 -0500

    login implementado
    Usando el envio la explicacion de AYER SE IMPLEMENTO EL LOGEO

--- RPCTest/Control/ControlLogin.cs
+++ RPCTest/Control/ControlLogin.cs
@@ -7,47 +7,53 @@ using AustinHarris.JsonRpc;
 using System.Data;
 using ADODBFactory;
 using System.Data.Entity;
-
+using Dominio;
 
 
 namespace Control
 {
     public class ControlLogin : JsonRpcService
     {
-        private IndicadoresEntityModel bd = new IndicadoresEntityModel();
         [JsonRpcMethod]
-        private string ValidarLogin(string usuario,string pass){
-          
-            var qrydb = from user in bd.USUARIOS
-                            where user.NOMBRE == usuario && user.PASSWORD == pass
-                            select user;
-            if (qrydb != null)
-            {
-                return "Usuario no Existe";
-            }
-            else return "Login Ok";            
-        }
-
-        [JsonRpcMethod]
-        private string RegistrarLogin(string usuario,string nom,string apepat,string apemat,string email,string direc,string dni,string pass)
+        public List<Usuario> ValidarLogin(string usu, string pass)
         {
-
-            using (var bd1 = new IndicadoresEntityModel())
+            using (var bd = new IndicadoresEntityModel())
             {
-                USUARIOS _usu = new USUARIOS();
-                _usu.NOMBRE = nom;
-                _usu.APEPAT = apepat;
-                _usu.APEMAT = apemat;
-                _usu.DIRECCION = direc;
-                _usu.DNI = dni;
-                _usu.EMAIL = email;
-                _usu.CODUSUARIO = usuario;
-                bd.USUARIOS.Attach(_usu);
-                /*log.IDDET = cc.ID;*/
-                bd1.SaveChanges();
-                return "Registro OK";
+                var qrydb = from user in bd.USUARIOS
+                            where user.EMAIL == usu && user.PASSWORD == pass
+                            select new Usuario() { IDUSUARIO = user.IDUSUARIO, CODUSUARIO = user.CODUSUARIO, APEPAT = user.APEPAT, APEMAT = user.APEMAT, NOMBRE = user.NOMBRE, EMAIL = user.EMAIL, DIRECCION = user.DIRECCION, DNI = user.DNI, PASSWORD = user.PASSWORD };
+                if (qrydb.Count() > 0)
+                {
+                    return qrydb.ToList(); //ejecuta
+
+                }
+                else
+                {
+
+                return null; //wilmer expresate
+                }
             }
-            
         }
+        //[JsonRpcMethod]
+        //private string RegistrarLogin(string usuario,string nom,string apepat,string apemat,string email,string direc,string dni,string pass)
+        //{
+
+        //    using (var bd1 = new IndicadoresEntityModel())
+        //    {
+        //        USUARIOS _usu = new USUARIOS();
+        //        _usu.NOMBRE = nom;
+        //        _usu.APEPAT = apepat;
+        //        _usu.APEMAT = apemat;
+        //        _usu.DIRECCION = direc;
+        //        _usu.DNI = dni;
+        //        _usu.EMAIL = email;
+        //        _usu.CODUSUARIO = usuario;
+        //        bd.USUARIOS.Attach(_usu);
+        //        /*log.IDDET = cc.ID;*/
+        //        bd1.SaveChanges();
+        //        return "Registro OK";
+        //    }
+            
+        //}
     }
 }
--- RPCTest/Dominio/Usuario.cs
+++ RPCTest/Dominio/Usuario.cs
@@ -3,82 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Dominio
 {
-    class Usuario
-    {
-        private decimal idUsuario;
+   public class Usuario
+   {
+       public decimal IDUSUARIO
+       {
+           get;
+           set;
+       }
 
-        public decimal IdUsuario
-        {
-            get { return idUsuario; }
-            set { idUsuario = value; }
-        }
-        
-        private string codUsuario;
+       public string CODUSUARIO
+       {
+           get;
+           set;
+       }
 
-        public string CodUsuario
-        {
-            get { return codUsuario; }
-            set { codUsuario = value; }
-        }
-        
-        private string apePat;
+       public string APEPAT
+       {
+           get;
+           set;
+       }
 
-        public string ApePat
-        {
-            get { return apePat; }
-            set { apePat = value; }
-        }
-        
-        private string apeMat;
+       public string APEMAT
+       {
+           get;
+           set;
+       }
 
-        public string ApeMat
-        {
-            get { return apeMat; }
-            set { apeMat = value; }
-        }
-        
-        private string nombre;
+       public string NOMBRE
+       {
+           get;
+           set;
+       }
 
-        public string Nombre
-        {
-            get { return nombre; }
-            set { nombre = value; }
-        }
-        
-        private string email;
+       public string EMAIL
+       {
+           get;
+           set;
+       }
 
-        public string Email
-        {
-            get { return email; }
-            set { email = value; }
-        }
-        
-        private string direccion;
+       public string DIRECCION
+       {
+           get;
+           set;
+       }
 
-        public string Direccion
-        {
-            get { return direccion; }
-            set { direccion = value; }
-        }
-        
-        private string dni;
+       public string DNI
+       {
+           get;
+           set;
+       }
 
-        public string Dni
-        {
-            get { return dni; }
-            set { dni = value; }
-        }
-        
-        private string password;
-
-        public string Password
-        {
-            get { return password; }
-            set { password = value; }
-        }
-
-    }
+       public string PASSWORD
+       {
+           get;
+           set;
+       }
+   }
 }
