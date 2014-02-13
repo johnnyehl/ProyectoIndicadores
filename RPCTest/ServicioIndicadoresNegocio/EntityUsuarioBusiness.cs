8b3613e951bbd84dd6d7545e282d290dacd8834d
Wilmer Cotrina Barzola <v_sigma@hotmail.com>
2014-02-13 16:41:00 -0500

    todos los metodos implementados
    sss

--- RPCTest/ServicioIndicadoresNegocio/EntityUsuarioBusiness.cs
+++ RPCTest/ServicioIndicadoresNegocio/EntityUsuarioBusiness.cs
@@ -114,5 +114,79 @@ namespace ServicioIndicadoresNegocio
                 return blogNames;
             }
         }
+        // consulta hecha por tobias -------------------------------------------------------
+        // coonsulta 2
+
+        [JsonRpcMethod]
+        public List<GESTION_INDICADORES_DETALLEbasecerrado> GestionIndicadoresCierre(int anho, int mes, String codobj, String atributo)
+        {
+            using (var context = new IndicadoresEntities())
+            {
+                var Valores2 = context.Database.SqlQuery<GESTION_INDICADORES_DETALLEbasecerrado>(
+                "SELECT " + atributo + ",txt_06,SUM(VAL_09) MARCACIONES,SUM(VAL_04) POTENCIAL, "
+       + " sum(CASE WHEN TXT_02 = 'Fijo'    THEN VAL_09 ELSE 0 END) MARCACIONES_FIJA, "
+       + "sum(CASE WHEN TXT_02 = 'Celular' THEN VAL_09 ELSE 0 END) MARCACIONE_CELULAR,  "
+       + " (sum(CASE WHEN TXT_02 = 'Fijo'    THEN VAL_09 ELSE 0 END)/((SELECT SUM(X.VAL_01+X.VAL_02+X.VAL_03) TRAMITADOS FROM GESTION_INDICADORES_DETALLE x where tipo = 'TABLA_BASES_UNICOS'  AND X.CODOBJ = Z.CODOBJ AND ESTADO = 'A' AND X.MES=Z.MES AND x.txt_06 = Z.TXT_06 ))) INTENTOS_FIJO, "
+       + "(sum(CASE WHEN TXT_02 = 'Celular' THEN VAL_09 ELSE 0 END)/((SELECT SUM(X.VAL_01+X.VAL_02+X.VAL_03) TRAMITADOS FROM GESTION_INDICADORES_DETALLE x where tipo = 'TABLA_BASES_UNICOS'  AND X.CODOBJ = Z.CODOBJ AND ESTADO = 'A' AND X.MES=Z.MES AND x.txt_06 = Z.TXT_06 ))) INTENTOS_CELULAR, "
+       + " (SUM(VAL_09)/((SELECT SUM(X.VAL_01+X.VAL_02+X.VAL_03) TRAMITADOS FROM GESTION_INDICADORES_DETALLE x where tipo = 'TABLA_BASES_UNICOS'  AND X.CODOBJ = Z.CODOBJ AND ESTADO = 'A' AND X.MES=Z.MES AND x.txt_06 = Z.TXT_06 ))) REINTENTOS "
++ " FROM GESTION_INDICADORES_DETALLE z "
++ " where tipo = 'TABLA_BASES_GESTION' "
++ " AND CODOBJ = '" + codobj + "'"
++ " AND MES=" + mes
++ " AND ANHO=" + anho
++ " group by z." + atributo + ",z.txt_06,z.CODOBJ,z.mes"
++ " order by 1 ").ToList();
+                return Valores2;
+            }
+        }
+
+
+
+        ///consulta 1
+        [JsonRpcMethod]
+        public List<GESTION_INDICADORES> GestionIndicadores(int anho, int mes, String codobj, String atributo)
+        {
+            using (var context = new IndicadoresEntities())
+            {
+                var Valores = context.Database.SqlQuery<GESTION_INDICADORES>(
+                " SELECT " + atributo + ",txt_06,SUM(VAL_09) TOTAL_BASE, "
+  + " SUM(VAL_03) CEF, "
++ "  SUM(VAL_02) CNE, "
++ "  SUM(VAL_01) NOC, "
++ "  SUM(VAL_01+VAL_02+VAL_03) TRAMITADOS, "
++ "  SUM(CASE WHEN FECHA IS NULL THEN VAL_09 ELSE 0 END) PENDIENTES, "
++ "  SUM(VAL_03+VAL_02) CONTACTOS, "
++ "  SUM(VAL_04) CONTACTOS_POTENCIAL, "
++ "  (SUM(VAL_04)/SUM(VAL_01+VAL_02+VAL_03))*100 CONTACTABILIDAD_POTENCIAL, "
++ "  (SUM(VAL_03)/SUM(VAL_01+VAL_02+VAL_03))*100 EFECTIVIDAD, "
++ "  (SUM(VAL_03)/SUM(VAL_02+VAL_03))*100 EFECT_CONTACTO, "
++ "  (SUM(VAL_03)/SUM(VAL_04))*100 EFECT_POTENCIAL "
++ " FROM GESTION_INDICADORES_DETALLE "
++ " where tipo = 'TABLA_BASES_UNICOS' "
+ + " AND CODOBJ = '" + codobj + "'"
++ " AND ESTADO = 'A' "
++ " AND MES= " + mes
++ " AND ANHO= " + anho
++ " group by " + atributo + ",txt_06 "
++ " order by 1").ToList();
+                return Valores;
+            }
+        }
+        [JsonRpcMethod]
+        public List<GESTION_INDICADORES_RESUMEN> GestionIndicadoresResumen(String codobj, int anho, int mes, int dia)
+        {
+            using (var cnx = new IndicadoresEntities())
+            {
+                var resumen = cnx.Database.SqlQuery<GESTION_INDICADORES_RESUMEN>(
+                    " select DESCRIPCION, VALOROBJ, LIMA, PROVINCIA "
+                    + " from gestion_indicadores_resumen"
+                    + " where CODOBJ = '" + codobj + "'"
+                    + " and ANHO = " + anho
+                    + " and MES = " + mes
+                    + " and DIA = " + dia).ToList();
+                return resumen;
+            }
+
+        }
     }
 }
---
+++ RPCTest/ServicioIndicadoresNegocio/GESTION_INDICADORES_RESUMEN.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioIndicadoresNegocio
+{
+    public class GESTION_INDICADORES_RESUMEN
+    {
+
+        public string DESCRIPCION { get; set; }
+        public decimal VALOROBJ { get; set; }
+        public decimal LIMA { get; set; }
+        public decimal PROVINCIA { get; set; }
+    }
+}
